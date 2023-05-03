e2d2e860415cc6cf62c36f897e97346b3d937c3e
LucasMartinelli <lucasbmartinelli@gmail.com>
2023-05-03 01:37:28 -0300

    home page update

--- src/app/pages/home/home-page/home-page.component.ts
+++ src/app/pages/home/home-page/home-page.component.ts
@@ -1,12 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {} from 'googlemaps';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, AfterViewInit {
+  @ViewChild('map') mapElement: any;
+  map?: google.maps.Map;
+
   constructor() {}
 
   ngOnInit() {}
+
+  ngAfterViewInit(): void {
+    const myLatLng = { lat: -23.670011, lng: -46.6694602 };
+    const mapProperties = {
+      center: { lat: -23.6694, lng: -46.6694602 },
+      zoom: 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+    };
+    const map = new google.maps.Map(
+      this.mapElement.nativeElement,
+      mapProperties
+    );
+
+    const image = '../../../../assets/imgs/marker.svg';
+
+    const marker = new google.maps.Marker({
+      position: myLatLng,
+      map,
+      icon: image,
+    });
+
+    this.map = map;
+  }
 }
